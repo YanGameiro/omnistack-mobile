ebcd5b5f80154865a9a54f7ed1da81bdd7f5788e
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-06 09:18:35 -0300

    inserting image on List.js (unfinished)

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Image, AsyncStorage } from 'react-native';
+
+import logo from '../assets/logo1.png'
 
 const List = () => {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storedTechs => {
+            const techsArray = storedTechs.split(',').map(tech => tech.trim());
+            setTechs(techsArray);
+        });
+    }, []);
+
     return (
-        <View />
+        <SafeAreaView >
+            <Image source={logo}/>
+        </SafeAreaView>
     )
 }
 
