a81c0369ebb0e78ad3cdeb62aa0a8263840c9a36
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-06 07:59:57 -0300

    incrementing style of login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,78 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Image, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import logo from '../assets/logo1.png';
 
 const Login = () => {
     return (
-        <View>
-            <Text> A from login!</Text>
-        </View>
-    )
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <Text>aaaaaaaa</Text>
+            <Image source={logo}></Image>
+            <View style={styles.form}>
+                <Text style={styles.label}>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Your Email'
+                    placeholderTextColor='#999999'
+                    keyboardType='email-address'
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <Text style={styles.label}>Techs</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Techs that you use'
+                    placeholderTextColor='#999999'
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Find Spots</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#444444',
+        marginBottom: 8,    
+    },
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop:  30,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#dddddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#B800AE',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+});
 
 export default Login;
